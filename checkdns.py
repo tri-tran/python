860b6db321382e9472e9a8bdb78ee7d5512d1b77
Tri <tri.tran@u.nus.edu>
2018-05-21 01:09:04 +0800

    Create CSV with domain, CNAME, A, AAAA

--- checkdns.py
+++ checkdns.py
@@ -1,13 +1,36 @@
 import dns.resolver
+import csv
 
 domains = []
 with open('domain.list') as f:
     domains = [x.strip() for x in f.readlines()]
 
-for d in domains:
-    print(d)
-    cname_data = dns.resolver.query(d, 'CNAME')
-    cnames = [str(x) for x in cname_data]
+def nslookup(domain, r_type):
+    try:
+        r_data = dns.resolver.query(domain, r_type)
+    except:
+        return '-'
 
-    print(d, cnames)
-    break
+    #print(domain, list(r_data))
+
+    return ';'.join([str(x).endswith('.') and str(x)[:-1] or str(x) for x in r_data])
+
+with open('result.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    for d in domains:
+        cname = ''
+        a_record = ''
+        aaaa_record = ''
+
+        if d.startswith('www'):
+            cname = nslookup(d, 'CNAME')
+            a_record = nslookup(d[4:], 'A')
+            aaaa_record = nslookup(d[4:], 'AAAA')
+        else:
+            cname = nslookup('www.' + d, 'CNAME')
+            a_record = nslookup(d, 'A')
+            aaaa_record = nslookup(d, 'AAAA')
+        
+        to_print = [d, cname, a_record, aaaa_record]
+        writer.writerow(to_print)
+
